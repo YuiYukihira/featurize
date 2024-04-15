609bb3c9660eb2e3906ddb5d5a1670116a0f3f99
Lucy Ekaterina <lucy@dragnof.pro>
2024-04-15 21:56:51 +0000

    ci: Automatically release artifacts
    Signed-off-by: Lucy Ekaterina <lucy@dragnof.pro>

--- nix/auth/apps.nix
+++ nix/auth/apps.nix
@@ -3,4 +3,12 @@ let
   inherit (inputs) nixpkgs std;
   l = nixpkgs.lib // builtins;
 in
-{ auth = inputs.flake-utils.lib.mkApp { drv = cell.packages.auth; }; }
+{
+  auth = inputs.flake-utils.lib.mkApp {
+    drv = nixpkgs.writeShellScriptBin "auth" ''
+      export TEMPLATES_DIR=${cell.packages.auth}/share/templates
+      export PUBLIC_DIR=${cell.packages.auth}/share/public
+      ${cell.packages.auth}/bin/auth
+    '';
+  };
+}
